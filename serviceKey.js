396350e7c8a20b02efb824a9f79785bb884c7f2b
HiteshGit001 <hiteshgn009@gmail.com>
2024-05-11 19:04:26 +0530

    service key fiexee

--- serviceKey.js
+++ serviceKey.js
@@ -1,14 +1,14 @@
 const serviceKey = {
   "type": "service_account",
   "project_id": "foodorderapp-18590",
-  "private_key_id": "721b8998b05afddec8c0bb0283456d5d13a22235",
+  "private_key_id": process.env.SERVICE_PRIVATE_KEY_ID,
   "private_key": process.env.SERVICE_PRIVATE_KEY,
   "client_email": "firebase-adminsdk-zrpv5@foodorderapp-18590.iam.gserviceaccount.com",
-  "client_id": "113199576959650127480",
+  "client_id": process.env.SERVICE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zrpv5%40foodorderapp-18590.iam.gserviceaccount.com",
+  "client_x509_cert_url": process.env.SERVICE_CLIENT_PROVIDER_CERT,
   "universe_domain": "googleapis.com",
   "storageBucket": "gs://foodorderapp-18590.appspot.com"
 }
