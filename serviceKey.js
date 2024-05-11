cd93254183076d749269431e33086e2b262729f8
HiteshGit001 <hiteshgn009@gmail.com>
2024-05-11 19:13:41 +0530

    fixess of env

--- serviceKey.js
+++ serviceKey.js
@@ -1,5 +1,5 @@
 const serviceKey = {
-  "type": "service_account",
+  "type": process.env.SERVICE_TYPE,
   "project_id": process.env.SERVICE_PROJECT_ID,
   "private_key_id": process.env.SERVICE_PRIVATE_KEY_ID,
   "private_key": process.env.SERVICE_PRIVATE_KEY,
